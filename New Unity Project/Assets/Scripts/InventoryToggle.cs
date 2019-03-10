61fcd8cd4f2f61386ff9005e681cbde7bfa28f18
Fraser <frasalex1@gmail.com>
2019-03-10 04:45:09 +0000

    Added Inventory and some art

---
+++ New Unity Project/Assets/Scripts/InventoryToggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryToggle : MonoBehaviour
+{
+    public static bool InventoryisOpen = false;
+
+    public GameObject Inv;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (InventoryisOpen == false)
+            {
+                InventoryisOpen = true;
+                Inv.SetActive(true);
+            }
+
+            else
+            {
+                InventoryisOpen = false;
+                Inv.SetActive(false);  
+            }               
+        }
+    }  
+
+}
