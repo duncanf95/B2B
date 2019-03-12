7692812d929251057a53a3b655d4155c185691d7
duncan <youvebeenvloged@gmail.com>
2019-03-12 13:57:33 +0000

    darked out rooms and torch

---
+++ New Unity Project/Assets/rooftriger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class rooftriger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<TilemapRenderer>().sortingLayerName = "Roof";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameObject.GetComponent<TilemapRenderer>().sortingLayerName = "Default";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+   
+        if (collision.CompareTag("Player"))
+        {
+            gameObject.GetComponent<TilemapRenderer>().sortingLayerName = "Roof";
+        }
+    }
+}
